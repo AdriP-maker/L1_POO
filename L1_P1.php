b272e1ea3750f62e365299b42e73cbbe107bdc6f
Mipro <Mipro@DESKTOP-QN52C2T>
2026-04-21 22:33:45 -0500

    Cambios de los archivos del progrma 1 Kotowa Mood

---
+++ L1_P1.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cafetería UTP - Realizar Pedido</title>
+    <!-- Bootstrap 3.4.1 CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/estilos.css">
+</head>
+<body>
+    <div class="container contenedor">
+        <?php include("L1_P1/html/header.html"); ?>
+
+
+        <main>
+            <?php include("L1_P1/html/menu.html"); ?>
+            <?php include("L1_P1/html/form.html"); ?>
+        </main>
+
+        <?php include("L1_P1/html/footer.html"); ?>
+    </div>
+
+    <!-- jQuery (obligatorio para Bootstrap 3) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <!-- Bootstrap 3.4.1 JS -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</body>
+
+</html>
