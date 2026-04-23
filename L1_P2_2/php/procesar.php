442c5f27c093108dfd68b2a44aecad9e703edc5a
Dion507 <geraderex01@gmail.com>
2026-04-23 02:27:17 -0500

    agregue mensaje de alerta

--- L1_P2_2/php/procesar.php
+++ L1_P2_2/php/procesar.php
@@ -1,11 +1,10 @@
 <?php
 include("ClasePedidoEscolar.php");
-
-if($_POST['lib1']==0&&$_POST['lib5']==0&&$_POST['boliAzul']==0&&$_POST['boliNeon']==0&&$_POST['regla']==0&&$_POST['borrador']==0&&$_POST['sacPuntas']==0){
+try {
+    if($_POST['lib1']==0&&$_POST['lib5']==0&&$_POST['boliAzul']==0&&$_POST['boliNeon']==0&&$_POST['regla']==0&&$_POST['borrador']==0&&$_POST['sacPuntas']==0){
     throw new Exception("Debe seleccionar al menos un producto"); 
 } else {
-    try { // INICIA TRY CATCH
-        $datos = [ // INICIALIZA ARREGLO 
+    $datos = [ // INICIALIZA ARREGLO 
         'libreta1' => $_POST['lib1'],
         'libreta5' => $_POST['lib5'],
         'boligrafoAzul' => $_POST['boliAzul'],
@@ -27,9 +26,11 @@ if($_POST['lib1']==0&&$_POST['lib5']==0&&$_POST['boliAzul']==0&&$_POST['boliNeon
     $totalCompra = $calcular -> obtenerTotalCompra(); // obtener total compra
 
     include("../../L1_P2_2.php");
-    
-    } catch(Exception $e){ // Termina try catch
-        echo $e->getMessage();
-    }
 }
+} catch(Exception $e){ // Termina try catch
+        echo "<script>alert('" . $e->getMessage() . "');</script>";
+        // 3. Si ocurre un error, guardamos el mensaje en una variable
+        include("../../L1_P2_2.php");
+    }
+
 ?>
