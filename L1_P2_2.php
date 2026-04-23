31a7ff8e25ccb4e4398ba0ae98f25f9a3a8cd83a
TqOliver2 <oliver.gonzalez6@utp.ac.pa>
2026-04-23 02:13:01 -0500

    Includes

--- L1_P2_2.php
+++ L1_P2_2.php
@@ -23,31 +23,7 @@
 				</div>
 				<!-- INICIA HEADER-->
 				<!-- INICIA NAV-->
-				<nav class="navbar navbar-default" id="navegacion" role="navigation">
-					<div class="container-fluid">
-						<!-- Brand and toggle get grouped for better mobile display -->
-						<div class="navbar-header">
-							<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-								<span class="sr-only">Toggle navigation</span>
-								<span class="icon-bar"></span>
-								<span class="icon-bar"></span>
-								<span class="icon-bar"></span>
-							</button>
-							<a class="navbar-brand" href="#">Escolar</a>
-						</div>
-						
-						<!-- Collect the nav links, forms, and other content for toggling -->
-						<div class="collapse navbar-collapse navbar-ex1-collapse">
-							<ul class="nav navbar-nav">
-								<li ><a href="./L1_P1.php">Programa 1</a></li>
-								<li class="active"><a href="L1_P2_2.php">Programa 2 - Miguel Gómez</a></li>
-								<li ><a href="./L1_P2_1.php">Programa 2 - Jose Ortega</a></li>
-								<li ><a href="./L1_P3.php">Programa 3</a></li>
-							</ul>
-							
-						</div><!-- /.navbar-collapse -->
-					</div>
-				</nav>
+				<?php include("L1_P2_2/html/navEscolarP2_2.html"); ?>
 				<!-- TERMINA NAV-->
 				
 				<!-- INICIA MAIN-->
@@ -59,66 +35,8 @@
 						<div class="row">
 
 							<!-- INICIO: Columna 1 -->
-							<div class="col-md-6">
-								<div class="panel panel-default">
-									<div class="panel-body" id="formulario"> <!-- INICIA PANEL-->
-										<div></div>
-										<form action="./L1_P2_2/php/procesar.php" method="POST" role="form"> <!-- INCIA FORM -->
-											<legend><h1>Programa 2</h1></legend>  
-											<fieldset> <!-- INCIA CAMPO CUADERNOS -->
-												<div class="panel panel-default"> <!-- INCIA CAMPO CUADERNOS PANEL -->
-													<div class="panel-body">
-														<LABEl>Cuadernos</LABEl>
-														<br>
-														<br>
-														Libreta 1 materia 
-														<input type="number" name="lib1" id="input" class="form-control" value="0" min="0" max="" step="" required="required" title="">
-														<br>
-														Libreta 5 materias 
-														<input type="number" name="lib5" id="input" class="form-control" value="0" min="0" max="" step="" required="required" title="">
-													</div> 
-												</div><!-- TERMINA CAMPO CUADERNOS PANEL -->
-											</fieldset> <!-- TERMINA CAMPO CUADERNOS -->
-											
-											<fieldset> <!-- INCIA CAMPO Bolígrafos  -->
-												<div class="panel panel-default"> <!-- INCIA CAMPO Bolígrafos PANEL -->
-													<div class="panel-body">
-														<LABEl>Bolígrafos</LABEl>
-														<br>
-														<br>
-														Paquete Bolígrafo azul 
-														<input type="number" name="boliAzul" id="input" class="form-control" value="0" min="0" max="" step="" required="required" title="">
-														<br>
-														Bolígrafo Neon  
-														<input type="number" name="boliNeon" id="input" class="form-control" value="0" min="0" max="" step="" required="required" title="">
-													</div> 
-												</div><!-- TERMINA CAMPO Bolígrafos PANEL -->
-											</fieldset> <!-- TERMINA CAMPO Bolígrafos -->
-
-											<fieldset> <!-- INCIA CAMPO Accesorios  -->
-												<div class="panel panel-default"> <!-- INCIA CAMPO Accesorios PANEL -->
-													<div class="panel-body">
-														<LABEl>Accesorios </LABEl>
-														<br>
-														<br>
-														Regla 30 cm 
-														<input type="number" name="regla" id="input" class="form-control" value="0" min="0" max="" step="" required="required" title="">
-														<br>
-														Borrador
-														<input type="number" name="borrador" id="input" class="form-control" value="0" min="0" max="" step="" required="required" title="">
-														<br>
-														Sacapuntas
-														<input type="number" name="sacPuntas" id="input" class="form-control" value="0" min="0" max="" step="" required="required" title="">
-													</div> 
-												</div><!-- TERMINA CAMPO Accesorios PANEL -->
-											</fieldset> <!-- TERMINA CAMPO Accesorios -->
-
-											<button type="submit" class="btn btn-primary">Comprar</button>  <!-- BOTON SUBMIT -->
-											
-										</form> <!-- INCIA FORM -->
-									</div> <!-- TERMINA PANEL-->
-								</div>
-							</div>
+							<?php include("L1_P2_2/html/menu_p2_2.html"); ?>
+							
 							<!-- FIN: Columna 1 -->
 
 							<!-- INICIO: Columna 2 -->
@@ -180,22 +98,7 @@
 				<!-- TERMINA MAIN-->
 
 				<!-- INICIA FOOTER-->
-				<div class="panel panel-default">
-					<div class="panel-body" id="panel-footer">
-						<img src="./L1_P2_2/img/logo-utp.png" class="img-logo-footer">
-						<div class="contenido-footer">
-							<h3>Universidad Tecnológica de Panamá</h3>
-							<h4>Centro Regional De Cocle</h4>
-							<h4>Licenciatura en Desarrollo y Gestión de Software</h4>
-							<h4>Ing. María Yahaira Tejedor M. 	</h4>
-							
-							<h4>Laboratorio: Elementos de la POO y manejo de excepciones en PHP</h4>
-						</div>
-					</div>
-					<div class="panel-footer" id="panelFooter">
-						<p>Miguel Gomez - Daniel Ubarte - Oliver Gonzales - Gerardo Tejada</p>
-					</div>
-				</div>
+				<?php include("L1_P2_2/html/footerP2_2.html"); ?>
 				<!-- TERMINA FOOTER-->
 				
 				<!-- jQuery -->
