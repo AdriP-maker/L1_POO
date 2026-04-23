29346e51f443baa9b8a95f6bda7cf9cd7aa1a848
TqOliver2 <oliver.gonzalez6@utp.ac.pa>
2026-04-23 02:38:55 -0500

    Arreglando inclide

--- L1_P2_2.php
+++ L1_P2_2.php
@@ -28,72 +28,7 @@
 				
 				<!-- INICIA MAIN-->
 				<main>
-					<!-- INICIO: Contenedor principal -->
-					<div class="container">
-
-						<!-- INICIO: Fila -->
-						<div class="row">
-
-							<!-- INICIO: Columna 1 -->
-							<?php include("L1_P2_2/html/menu_p2_2.html"); ?>
-							
-							<!-- FIN: Columna 1 -->
-
-							<!-- INICIO: Columna 2 -->
-							<div class="col-md-6">
-								<div> <!-- INICIA OUTPUT -->
-									<div class="panel panel-default">
-										<div class="panel-body" id="panel-output">
-											<legend><H1>Total</H1></legend>
-											<div class="table-responsive"> <!-- INICIA TABLA -->
-												<table class="table table-hover">
-													<thead>
-														<tr>
-															<th>Articulo</th>
-															<th>Cantidad</th>
-															<th>Precio unitario</th>
-															<th>Total</th>
-														</tr>
-													</thead>
-													<tbody>
-														<?php
-														try {
-															if (isset($articulos)) {
-																foreach ($articulos as $fila) {
-																	echo "<tr>";
-																	echo "<td>" . $fila[0] . "</td>";
-																	echo "<td>" . $fila[1] . "</td>";
-																	echo "<td>" . number_format($fila[2], 2) . "</td>";
-																	echo "<td>" . number_format($fila[3], 2) . "</td>";
-																	echo "</tr>";
-																}
-																echo "<tr>";
-																echo "<td><h4><strong>Sub total:</strong></h4>".number_format($subtotal, 2). "</td>";
-																echo "<td><h4><strong>Total de compra:</strong></h4>".number_format($totalCompra, 2). "</td>";
-																echo "</tr>";
-															}
-														} catch(Exception $e){ // Termina try catch
-															echo $e->getMessage();
-														}
-														?>
-													</tbody>
-												</table>
-											</div>  <!-- INICIA TABLA -->
-										</div>
-									</div>
-								</div> <!-- TERMINA OUTPUT -->
-							</div>
-							<!-- FIN: Columna 2 -->
-						</div>
-						<!-- FIN: Fila -->
-
-					</div>
-					<!-- FIN: Contenedor principal -->
-
-
-					<div class="container-fluid"> <!-- INICIA CONTENEDOR-->
-						
-					</div> <!-- TERMINA CONTENEDOR-->
+					<?php include("contenidoMain.php"); ?>
 				</main>
 				<!-- TERMINA MAIN-->
 
